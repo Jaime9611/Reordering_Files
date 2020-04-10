daceaf0823124167b1b8e10948ad1be566b2028f
Jaime9611 <jaimeescobar9611@gmail.com>
2020-04-10 11:08:55 -0500

    Working

--- sort_files.py
+++ sort_files.py
@@ -6,7 +6,9 @@ from pathlib import Path
 
 logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
 
-LOCATION = './files_sorted'
+INPUT_DIR = './files'
+OUTPUT_DIR = './files_sorted'
+
 
 def create_dir(files_dir):
     try:
@@ -21,17 +23,30 @@ def create_dir(files_dir):
         files_dir.mkdir()
 
 
+def move_files(input_dir, output_dir):
+    for f in input_dir.iterdir():
+        new_file = output_dir / f'{f.stem.lower()}{f.suffix}'
+        logging.debug(new_file)
+        f.rename(new_file)
+
 
 def main():
     with open('data/data.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         
-        files_dir = Path(LOCATION)
-        create_dir(files_dir)
+        input_dir = Path(INPUT_DIR)
+        output_dir = Path(OUTPUT_DIR)
+        create_dir(output_dir)
+        move_files(input_dir, output_dir)
 
-        for row in reader:
-            file_name = row['PRODUCT'].lower()
-            logging.info(file_name)
+        for index, row in enumerate(reader, 1):
+            product_name = row['PRODUCT'].lower()
+            for f in output_dir.iterdir():
+                if f.stem == product_name:
+                    target = Path(output_dir / f'{index}_{product_name}{f.suffix}')
+                    f.rename(target)
+
+
 
 
 if __name__ == "__main__":
